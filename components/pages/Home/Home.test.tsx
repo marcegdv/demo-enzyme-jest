61ce226101a60383e924b4f458e50ab92055fdae
marcegdv <mgdv79@gmail.com>
2022-02-14 08:46:33 -0300

    .

--- components/pages/Home/Home.test.tsx
+++ components/pages/Home/Home.test.tsx
@@ -16,6 +16,8 @@ jest.mock('next/router', () => ({
 describe('Home component:', () => {
     const component: ShallowWrapper = shallow(<Home />);
     it('render specific subcomponents', () => {
+        /* Este test puede acceder a cada componente que se declaró
+           en el return() de <Home/> */
         expect(component.find('Nav')).toHaveLength(1);
         expect(component.find('Main')).toHaveLength(1);
         expect(component.find('MainCards')).toHaveLength(1);
@@ -25,20 +27,47 @@ describe('Home component:', () => {
         expect(component.find('DialogContent')).toHaveLength(0);
     });
     test('if MainCards render its content', () => {
-        expect(component.find('MainCards').dive().find(TextCard)).toHaveLength(2);
+        /* Pero no todos los componentes pueden ser accedidos como en
+           el test anterior, ya que algunos componentes no son parte
+           del return() de <Home/> que son retornados por otra función
+           que luego renderizará el return() de <Home/>, y se los
+           accede con la función dive() que retorna una nuevo
+           ShallowWrapper a partir de ese DOM */
         const textCards = component.find('MainCards').dive().find(TextCard);
         expect(textCards).toHaveLength(2);
         expect(textCards.at(0).prop('color')).toEqual('danger');
         expect(textCards.at(1).prop('color')).toEqual('warning');
     });
-    it('when click on DivButton, render Dialog', () => {
+    test('when click on DivButton, render Dialog', () => {
         component.find('DivButton').simulate('click');
+        expect(component.find('Dialog')).toHaveLength(1);
+        expect(component.find('DialogContent')).toHaveLength(1);
+    });
+    test('when Enter key press on DivButton, render Dialog', () => {
+        component.find('DivButton').simulate('keypress', { key: 'Enter' });
+        expect(component.find('Dialog')).toHaveLength(1);
+        expect(component.find('DialogContent')).toHaveLength(1);
+    });
+    test('when SpaceBar key press on DivButton, render Dialog', () => {
+        component.find('DivButton').simulate('keypress', { key: ' ' });
         expect(component.find('Dialog')).toHaveLength(1);
         expect(component.find('DialogContent')).toHaveLength(1);
     });
 });
 
 describe('When dialog is open:', () => {
+    beforeEach(() => {
+        mockRouter.mockReset();
+    });
+    /* Para testear si se le paso el contenido esperado al componente
+       <Dialog/>, es necesario renderizar a <Home/> con la función
+       mount(), la cual renderiza el componente que se le pasa como
+       parámetro y sus subcomponentes. En las siguientes tres líneas,
+       se monta el componente principal, se hace click sobre el botón
+       que muestra el cuadro de diálogo, y se buscan el componente
+       que renderiza el contenido del cuadro de dálogo. Luego se
+       verifica si contiene lo esperado y si cumple con el
+       funcionamiento deseado */
     const component: ReactWrapper = mount(<Home />);
     component.find('DivButton').simulate('click');
     const content: ReactWrapper = component.find('DialogContent');
@@ -47,8 +76,10 @@ describe('When dialog is open:', () => {
         expect(content.find('LinkButton')).toHaveLength(1);
     });
     test('when click on LinkButton, go to /contact', () => {
+        expect(mockRouter).toHaveBeenCalledTimes(0);
         mockRouter.mockReset();
         content.find('LinkButton').simulate('click');
+        expect(mockRouter).toHaveBeenCalledTimes(1);
         expect(mockRouter).toHaveBeenCalledWith('/about');
     });
 });
